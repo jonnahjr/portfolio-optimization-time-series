f7973b367fd310a33bc03ed37e3ccc87af967c59
jonnahjr <pronivamusic14@gmail.com>
2025-02-27 12:26:11 +0300

    Add configuration file for managing global settings

---
+++ src/config.py
@@ -0,0 +1,8 @@
+tsla_ticker = 'TSLA'
+selected_tickers = ['TSLA', 'BND', 'SPY']
+start_date = '2015-01-01'
+end_date = '2024-10-31'
+num_portfolios = 10000
+risk_free_rate = 0.01
+forecast_horizon = 365
+
