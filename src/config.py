1129c8ec5d2a84e398179246b4b26f370ab9e285
yonas bogale <pronivamusic14@gmail.com>
2025-02-28 11:06:19 +0300

    Delete src/config.py

--- src/config.py
+++
@@ -1,8 +0,0 @@
-tsla_ticker = 'TSLA'
-selected_tickers = ['TSLA', 'BND', 'SPY']
-start_date = '2015-01-01'
-end_date = '2024-10-31'
-num_portfolios = 10000
-risk_free_rate = 0.01
-forecast_horizon = 365
-
